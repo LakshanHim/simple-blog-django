b3a84f9592de3a3a57e7283e7d6e58e01610eb7a
LakshanHim <lakshan.him@gmail.com>
2025-02-27 23:18:16 +0530

    blog card create

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.homepage,name='home'),
     path('about/',views.about,name='about'),
+    path('content/<str:pk>/',views.content,name='content'),
+    path('contact/',views.contact,name='contact'),
 ]
